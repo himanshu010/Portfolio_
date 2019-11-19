b6f644e4ece6732ce8497c9b7628a8633d540b81
himanshu <him9711973697@gmail.com>
2019-11-19 15:44:21 +0530

    improving Discover more page

---
+++ discover_more.js
@@ -0,0 +1,16 @@
+let knowLine = document.getElementById('know_more');
+let button = document.getElementById('check');
+let flag = 0;
+button.onclick = clickCheck;
+
+// function clickCheck() {
+//     knowLine.style.display = 'none';
+// }
+function clickCheck() {
+    flag = flag + 1;
+    if (flag % 2 !== 0) {
+        knowLine.style.display = 'none';
+    } else {
+        knowLine.style.display = 'block';
+    }
+}
